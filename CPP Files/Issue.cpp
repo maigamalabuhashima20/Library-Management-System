a4e04c63f5caf2f6e9da03d8d38268c8eb7459cf
MryamAhmed <87723246+MryamAhmed@users.noreply.github.com>
2022-05-20 03:19:40 +0200

    Update Issue.cpp

--- CPP Files/Issue.cpp
+++ CPP Files/Issue.cpp
@@ -6,3 +6,14 @@ Issue::Issue()
 	id = 0;
 	Name[0] = 0;
 }
+
+Issue::Issue(short id, char cat[], char n[],short q, float p)
+{
+	id = 0;
+	category[0] = 0;
+	student_name[0] = 0;
+	Qty = 0;
+	date[0] = 0;
+}
+
+
