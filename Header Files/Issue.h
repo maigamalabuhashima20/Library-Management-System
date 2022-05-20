00f1e8734933d7bbff0c0d155526781f6dda629e
MryamAhmed <87723246+MryamAhmed@users.noreply.github.com>
2022-05-20 02:32:57 +0200

    Update Issue.h
    i added the class and all operators mryam

--- Header Files/Issue.h
+++ Header Files/Issue.h
@@ -1,1 +1,43 @@
-hh
+#include <iostream>
+//#include "fixtext.h"
+//#include "lentext.h"
+#include "varlen.h"
+
+using namespace std;
+
+class Issue
+{
+public:
+	// fields
+	short	id;
+	char	student_name[11];
+	short   Qty;
+	char    date[11];
+	char category[20];
+
+
+
+	void setId(short id);
+	void setCategory(char category[]);
+	void setstudent_name(char name[]);
+	void setQty(float qty);
+
+	// Getters
+	short getId() const;
+	char getCategory() const;
+	char getstudent_name() const;
+	short getQty() const;
+
+	
+	Issue();
+	Issue(short shId, char name[]);
+
+	void InitRecord(VariableLengthRecord&); //set Record  Configuration
+
+	int Pack(VariableLengthRecord&);	//  From ObjectPerson  to  Record =>  Write
+
+	int Unpack(VariableLengthRecord&);  // From Record to  Object Person  => Read
+
+	void Print(ostream&);
+
+};
