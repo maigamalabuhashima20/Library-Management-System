0ffb1df60bfa30525972ec904bd1ece75867320a
ManarHamdy <93196360+ManarHamdy@users.noreply.github.com>
2022-05-21 09:57:21 +0200

    Add files via upload

---
+++ Header Files/Book.h
@@ -0,0 +1,49 @@
+#include <iostream>
+//#include "fixtext.h"
+//#include "lentext.h"
+#include "varlen.h"
+
+using namespace std;
+
+class Book
+{
+private:
+	// fields
+	short id;
+	char category[20];
+	char Name[20];
+	char Author[20];
+	short Qty;
+	float price;
+
+public:
+	// Setters
+	void setId(short id);
+	void setCategory(const char* category);
+	void setName(const char* name);
+	void setAuthor(const char* author);
+	void setQty(short qty);
+	void setPrice(float price);
+
+	// Getters
+	short getId() const;
+	const char* getCategory() const;
+	const char* getName() const;
+	const char* getAuthor() const;
+	short getQty() const;
+	float getPrice() const;
+
+	// operations
+	Book();
+	Book(short shId, char* c, char* n, char* a, short q, float p); /// this is error here ??  now it's no error
+
+	void InitRecord(VariableLengthRecord&); // set Record  Configuration
+
+	int Pack(VariableLengthRecord&); //  From ObjectPerson  to  Record =>  Write
+
+	int Unpack(VariableLengthRecord&); // From Record to  Object Person  => Read
+
+	void Print(ostream&);
+	void Add_Book();
+	void Updata_Books();
+};
