1ef8f6022ca459b316688f21ba84fc8c2cf65850
maigamalabuhashima20 <mj1232@fayoum.edu.eg>
2022-05-22 17:24:09 +0200

    commit

--- CPP Files/book.cpp
+++ CPP Files/book.cpp
@@ -27,15 +27,15 @@ void Book::setId(short id)
 {
 	this->id = id;
 }
-void Book::setCategory(const char category[])
+void Book::setCategory(const char *category)
 {
 	strcpy_s(this->category, category);
 }
-void Book::setName(const char name[])
+void Book::setName(const char *name)
 {
 	strcpy_s(this->Name, name);
 }
-void Book::setAuthor(const char author[])
+void Book::setAuthor(const char *author)
 {
 	strcpy_s(this->Author, author);
 }
@@ -52,15 +52,15 @@ short Book::getId() const
 {
 	return id;
 }
-const char* Book::getCategory() const
+const char *Book::getCategory() const
 {
 	return category;
 }
-const char* Book::getName() const
+const char *Book::getName() const
 {
 	return Name;
 }
-const char* Book::getAuthor() const
+const char *Book::getAuthor() const
 {
 	return Author;
 }
@@ -74,7 +74,7 @@ float Book::getPrice() const
 }
 
 // initialize a VariableLengthRecord to be used for Books
-void Book::InitRecord(VariableLengthRecord& record)
+void Book::InitRecord(VariableLengthRecord &record)
 {
 	record.init(6);				  //#fields
 	record.AddField(0, 'F', 2);	  // id
@@ -86,82 +86,83 @@ void Book::InitRecord(VariableLengthRecord& record)
 }
 
 // this pack is for the person class
-int Book::Pack(VariableLengthRecord& record)
+int Book::Pack(VariableLengthRecord &record)
 {
 	// pack the fields into a VariableLengthRecord, return  ( 1 )TRUE if all succeed, FALSE o/w
 	int result, recordSize = 0; // Length Indicator  id(short) = 120 , name = AMES , Delimiter = 1 = > 7
 
 	// because we are length indicator record
-	recordSize = 11 + sizeof(Name) + sizeof(category) + sizeof(Author);
+	recordSize = 11 + strlen(Name) + strlen(category) + strlen(Author);
 
 	record.Clear(recordSize);
 
 	// result = record.Pack(0, (void *)&id, sizeof(short));
 
 	// but this pack is for the variable length record
-	result = record.Pack(0, (void*)&id, sizeof(short)) &&
-		record.Pack(1, (char*)category, sizeof(category)) &&
-		record.Pack(2, (char*)Name, sizeof(Name)) &&
-		record.Pack(3, (char*)Author, sizeof(Author)) &&
-		record.Pack(4, (void*)&Qty, sizeof(short)) &&
-		record.Pack(5, (void*)&price, sizeof(float));
+	result = record.Pack(0, (void *)&id, sizeof(short)) &&
+			 record.Pack(1, (void *)category, strlen(category)) &&
+			 record.Pack(2, (void *)Name, strlen(Name)) &&
+			 record.Pack(3, (void *)Author, strlen(Author)) &&
+			 record.Pack(4, (void *)&Qty, sizeof(short)) &&
+			 record.Pack(5, (void *)&price, sizeof(float));
 
 	return result;
 }
 
-int Book::Unpack(VariableLengthRecord& record)
+int Book::Unpack(VariableLengthRecord &record)
 {
 
 	// use true with array of char to tell us that text is done
 	int result;
-	result = record.Unpack(0, (char*)&id) && record.Unpack(1, category, true) &&
-		record.Unpack(2, Name, true) &&
-		record.Unpack(3, Author, true) &&
-		record.Unpack(4, (char*)&Qty) &&
-		record.Unpack(5, (char*)&price);
+	result = record.Unpack(0, (char *)&id) &&
+			 record.Unpack(1, category, true) &&
+			 record.Unpack(2, Name, true) &&
+			 record.Unpack(3, Author, true) &&
+			 record.Unpack(4, (char *)&Qty) &&
+			 record.Unpack(5, (char *)&price);
 	return result;
 }
 ////////////////////////////////////////////////////////////////////mrymhuobppup
-void Book::Print(ostream& stream)
+void Book::Print(ostream &stream)
 {
 	stream << "Book:"
-		<< "\tId '" << getId() << "'\n"
-		<< "\tcategory '" << getCategory() << "'\n"
-		<< "\tName '" << getName() << "'\n"
-		<< "\tAuthor '" << getAuthor() << "'\n"
-		<< "\tQty '" << getQty() << "'\n"
-		<< "\tprice '" << getPrice() << "'\n";
+		   << "\tId '" << getId() << "'\n"
+		   << "\tcategory '" << getCategory() << "'\n"
+		   << "\tName '" << getName() << "'\n"
+		   << "\tAuthor '" << getAuthor() << "'\n"
+		   << "\tQty '" << getQty() << "'\n"
+		   << "\tprice '" << getPrice() << "'\n";
 }
 void Book::Add_Book()
 {
 	VariableLengthRecord outRecord;
-	short id , choice = 1;
+	short id, choice = 1;
 	char category[20], Name[20], Author[20];
 	short Qty;
 	float price;
 
 	InitRecord(outRecord); // only once
-	ofstream TestOut("deltext.dat", ios::out | ios::binary | ios::app);
+	ofstream TestOut("books.dat", ios::out | ios::binary | ios::app);
 
-	outRecord.WriteHeader(TestOut);  // once
+	outRecord.WriteHeader(TestOut); // once
 	if (TestOut.is_open())
 	{
-		while(choice)
+		while (choice)
 		{
 			cout << "Enter the Book id\n";
 			cin >> id;
 			setId(id);
-
+			cin.ignore();
 			cout << "Enter the Book Category\n";
-			cin>>category;
+			cin.getline(category, 20);
 			setCategory(category);
 
 			cout << "Enter the Book Name\n";
-			cin>>Name;
+			cin.getline(Name, 20);
 			setName(Name);
 
 			cout << "Enter the Book Author\n";
-			cin>>Author;
+			cin.getline(Author, 20);
 			setAuthor(Author);
 
 			cout << "Enter the Book price\n";
@@ -170,7 +171,6 @@ void Book::Add_Book()
 			cout << "Enter the Book Qty\n";
 			cin >> Qty;
 			setQty(Qty);
-
 
 			Pack(outRecord);
 			outRecord.WriteL(TestOut);
@@ -182,10 +182,10 @@ void Book::Add_Book()
 	TestOut.close();
 }
 
-
-void Book::display_book() {
+void Book::display_book()
+{
 	VariableLengthRecord inRecord;
-	ifstream TestIn("deltext.dat", ios::in | ios::binary);
+	ifstream TestIn("books.dat", ios::in | ios::binary);
 	inRecord.ReadHeader(TestIn);
 	if (TestIn.is_open())
 	{
@@ -194,6 +194,45 @@ void Book::display_book() {
 			cout << "read " << inRecord.ReadL(TestIn) << endl;
 			cout << "unpack " << Unpack(inRecord) << endl;
 			Print(cout);
+		}
+	}
+	TestIn.close();
+}
+
+void Book::search_books()
+{
+	short id, choice = 1, flag = 0;
+	VariableLengthRecord inRecord;
+	ifstream TestIn("books.dat", ios::in | ios::binary);
+	inRecord.ReadHeader(TestIn);
+	if (TestIn.is_open())
+	{
+		while (choice)
+		{
+			cout << "Enter id of book to search for : ";
+			cin >> id;
+			// TestIn.seekg(0, ios::beg);
+			while (!TestIn.eof())
+			{
+				inRecord.ReadL(TestIn);
+				Unpack(inRecord);
+
+				if (getId() == id)
+				{
+					Print(cout);
+					flag = 1;
+					break;
+				}
+				else
+				{
+					flag = 0;
+					continue;
+				}
+			}
+			if (flag == 0)
+				cout << "Not Found\n";
+			cout << "want to search again ? enter 1 to search  or 0 to main menu ";
+			cin >> choice;
 		}
 	}
 	TestIn.close();
--- Header Files/book.h
+++ Header Files/book.h
@@ -1,3 +1,4 @@
+#pragma once
 #include <iostream>
 //#include "fixtext.h"
 //#include "lentext.h"
@@ -19,32 +20,33 @@ private:
 public:
 	// Setters
 	void setId(short id);
-	void setCategory(const char category[]);
-	void setName(const char name[]);
-	void setAuthor(const char author[]);
+	void setCategory(const char *category);
+	void setName(const char *name);
+	void setAuthor(const char *author);
 	void setQty(short qty);
 	void setPrice(float price);
 
 	// Getters
 	short getId() const;
-	const char* getCategory() const;
-	const char* getName() const;
-	const char* getAuthor() const;
+	const char *getCategory() const;
+	const char *getName() const;
+	const char *getAuthor() const;
 	short getQty() const;
 	float getPrice() const;
 
 	// operations
 	Book();
-	Book(short shId, char* c, char* n, char* a, short q, float p); /// this is error here ??  now it's no error
+	Book(short shId, char *c, char *n, char *a, short q, float p); /// this is error here ??  now it's no error
 
-	void InitRecord(VariableLengthRecord&); // set Record  Configuration
+	void InitRecord(VariableLengthRecord &); // set Record  Configuration
 
-	int Pack(VariableLengthRecord&); //  From ObjectPerson  to  Record =>  Write
+	int Pack(VariableLengthRecord &); //  From ObjectPerson  to  Record =>  Write
 
-	int Unpack(VariableLengthRecord&); // From Record to  Object Person  => Read
+	int Unpack(VariableLengthRecord &); // From Record to  Object Person  => Read
 
-	void Print(ostream&);
+	void Print(ostream &);
 	void Add_Book();
 	void Updata_Books();
 	void display_book();
+	void search_books();
 };
